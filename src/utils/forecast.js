20e6ea1d94adb60e6c06ab1994385f594814fa8a
Migz Suelto <migz.suelto@gmail.com>
2019-06-04 23:14:47 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/f40f45181b8b5bd1f0a81f71724f3edb/'+ longitude + ',' + latitude + '?units=si';
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            const currently = body.currently;
+            callback(undefined, body.daily.data[0].summary + " It is currently " + currently.temperature + " degrees out. There is a " + currently.precipProbability + "% change of rain.");
+        }
+    });
+}
+
+module.exports = forecast
