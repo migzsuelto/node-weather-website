049c2d0af3a58a3bded2e1474587662845c52c88
Migz Suelto <migz.suelto@gmail.com>
2019-06-04 23:31:48 +0800

    [ENH] Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -16,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = "Loading...";
     messageTwo.textContent = '';
-    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
@@ -27,5 +21,4 @@ weatherForm.addEventListener('submit', (e) => {
             }
         })
     })
-
 })
